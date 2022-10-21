1652c7d725452afda91b4bb27ddb3d8668ec38d3
krzyswys <krzyswys3@gmail.com>
2022-10-21 23:57:02 +0200

    updated tests

--- oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -1,5 +1,6 @@
 package agh.ics.oop;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,60 +10,90 @@ import static org.junit.jupiter.api.Assertions.*;
 class OptionsParserTest {
 
     @Test
-    void parseFull() {
+    @Order(1)
+    void parseFull(){
+        OptionsParserTest.sparseFull();
+    }
+    @Test
+    @Order(2)
+    void parseShort(){
+        OptionsParserTest.sparseShort();
+    }
+    @Test
+    @Order(3)
+    void parseUpperFull(){
+        OptionsParserTest.sparseUpperFull();
+    }
+    @Test
+    @Order(3)
+    void parseUpperShort(){
+        OptionsParserTest.sparseUpperShort();
+    }
+    @Test
+    @Order(4)
+    void parseMixed(){
+        OptionsParserTest.sparseMixed();
+    }
+    @Test
+    @Order(5)
+    void parseNoneMix(){
+        OptionsParserTest.sparseNoneMix();
+    }
+    @Test
+    @Order(6)
+    void parseNone(){
+        OptionsParserTest.sparseNone();
+    }
+     static void sparseFull() {
         String[] array = new String[]{"right", "left", "backward", "forward"};
-        List<MoveDirection> expected = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
-        assertEquals(expected, OptionsParser.parse(array));
+        List<MoveDirection> dir = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
+        assertEquals(dir, OptionsParser.parse(array));
     }
 
-    @Test
-    void parseShort() {
+
+    public static void sparseShort() {
         String[] array = new String[]{"r", "l", "b", "f"};
-        List<MoveDirection> expected = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
-        assertEquals(expected, OptionsParser.parse(array));
+        List<MoveDirection> dir = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
+        assertEquals(dir, OptionsParser.parse(array));
     }
 
-    @Test
-    void parseUpperFull() {
+    static void sparseUpperFull() {
         String[] array = new String[]{"RIGHT", "LEFT", "BACKWARD", "FORWARD"};
-        List<MoveDirection> expected = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
-        assertNotEquals(expected, OptionsParser.parse(array));
+        List<MoveDirection> dir = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
+        assertNotEquals(dir, OptionsParser.parse(array));
     }
 
-    @Test
-    void parseUpperShort() {
+    static void sparseUpperShort() {
         String[] array = new String[]{"R", "L", "B", "F"};
-        List<MoveDirection> expected = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
-        assertNotEquals(expected, OptionsParser.parse(array));
+        List<MoveDirection> dir = List.of(MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.FORWARD);
+        assertNotEquals(dir, OptionsParser.parse(array));
 
     }
 
-    @Test
-    void parseMixed() {
+    static void sparseMixed() {
         String[] array = new String[]{"right", "L", "backward", "string", "right", "f", "f", "asdasd", "null"};
-        List<MoveDirection> expected = List.of(
+        List<MoveDirection> dir = List.of(
                 MoveDirection.RIGHT,
                 MoveDirection.BACKWARD,
                 MoveDirection.RIGHT,
                 MoveDirection.FORWARD,
                 MoveDirection.FORWARD);
-        assertEquals(expected, OptionsParser.parse(array));
+        assertEquals(dir, OptionsParser.parse(array));
 
     }
 
-    @Test
-    void parseNoneMix() {
+    static void sparseNoneMix() {
         String[] array = new String[]{"rigsht", "L", "backwaards", "string", "rigfht", "af", "fs", "asdasd", "null"};
-        List<MoveDirection> expected = List.of();
-        assertEquals(expected, OptionsParser.parse(array));
+        List<MoveDirection> dir = List.of();
+        assertEquals(dir, OptionsParser.parse(array));
 
     }
 
-    @Test
-    void parseNone() {
+    static void sparseNone() {
         String[] array = new String[]{};
-        List<MoveDirection> expected = List.of();
-        assertEquals(expected, OptionsParser.parse(array));
+        List<MoveDirection> dir = List.of();
+        assertEquals(dir, OptionsParser.parse(array));
 
     }
+
 }
